4b06f675a892ae37dacbf2b896a99b82b9e324fa
Chemllei Joseph <chemllei0304@gmail.com>
2022-01-13 17:18:42 -0500

    Define model attrs.

--- PassGen/models.py
+++ PassGen/models.py
@@ -1,5 +1,5 @@
 from flask_login import UserMixin
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, Text, DateTime
 from sqlalchemy.orm import relationship
 
 from PassGen import db
@@ -9,6 +9,8 @@ class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
+    email = Column(Text)
+    password = Column(Text)
     accounts = relationship("Account", lazy="dynamic")
 
     def __init__(self, **kwargs):
@@ -19,6 +21,12 @@ class Account(db.Model):
     __tablename__ = "accounts"
 
     id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    user_ = Column(Text)
+    pass_ = Column(Text)
+    hint = Column(Text)
+    date_modified = Column(DateTime)
+    color = Column(Text)
     user_id = Column(Integer, ForeignKey("users.id"))
 
     def __init__(self, **kwargs):
